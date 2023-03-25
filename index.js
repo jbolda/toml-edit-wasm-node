286d8cf4a431d7e48504026596ab444a32b1005d
amrbashir <amr.bashir2015@gmail.com>
2023-03-26 00:34:29 +0200

    dynamic changes

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const { readFileSync } = require("fs");
 let file = readFileSync("./Cargo.toml", "utf8");
 console.log("\x1b[33mBefore\x1b[0m");
 console.log(file);
-let file_edit = edit(file);
+let file_edit = edit(file, "package > name", "new name");
 console.log("\x1b[33mAfter\x1b[0m");
 console.log(file_edit);
--- src/lib.rs
+++ src/lib.rs
@@ -3,14 +3,22 @@ use std::str::FromStr;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn edit(toml: String) -> Result<String, String> {
+pub fn edit(toml: String, path: String, new_value: String) -> Result<String, String> {
     let mut toml = toml_edit::Document::from_str(&toml).map_err(|e| e.to_string())?;
-    let package = toml
-        .get_mut("package")
-        .ok_or_else(|| "error: `package` couldn't be found")?;
-    let name = package
-        .get_mut("name")
-        .ok_or_else(|| "error: `name` couldn't be found")?;
-    *name = toml_edit::Item::Value(toml_edit::Value::from("Mutated Name"));
+    let mut path = path.split('>');
+    let key = path
+        .next()
+        .ok_or_else(|| "path must at least contain one component")?
+        .trim();
+    let mut key = toml
+        .get_mut(key)
+        .ok_or_else(|| format!("'{key}' couldn't be found"))?;
+    for k in path {
+        let k = k.trim();
+        key = key
+            .get_mut(k)
+            .ok_or_else(|| format!("'{k}' couldn't be found"))?;
+    }
+    *key = toml_edit::Item::Value(toml_edit::Value::from(new_value));
     Ok(toml.to_string())
 }
